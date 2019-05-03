2e82f1133061ac34f5b6ec4aa616b1978ca20048
farrukh90 <farrukhsadykov@gmail.com>
2019-05-02 19:58:03 -0500

    added index to /tmp

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,4 +5,7 @@ node{
     stage("Webserver Install"){
         sh "ssh  ec2-user@34.254.229.99       sudo yum install httpd -y"
     }
+    stage("Index file"){
+        sh "scp  index.html          ec2-user@34.254.229.99:/tmp"
+    }
 }
