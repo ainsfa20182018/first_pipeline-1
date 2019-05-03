3d97f5789b76881abe13c75a73c8020411f2f011
farrukh90 <farrukhsadykov@gmail.com>
2019-05-02 20:32:22 -0500

    IP changed to environment

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,15 +5,15 @@ node{
         git 'git@github.com:farrukh90/cool_website.git'
     }
     stage("Webserver Install"){
-        sh "ssh  ec2-user@34.254.229.99       sudo yum install httpd -y"
+        sh "ssh  ec2-user@${Environment}       sudo yum install httpd -y"
     }
     stage("Index file"){
-        sh "scp  index.html          ec2-user@34.254.229.99:/tmp"
+        sh "scp  index.html          ec2-user@${Environment}:/tmp"
     }
     stage("Move index"){
-        sh "ssh  ec2-user@34.254.229.99     sudo mv /tmp/index.html        /var/www/html/index.html"
+        sh "ssh  ec2-user@${Environment}     sudo mv /tmp/index.html        /var/www/html/index.html"
     }
     stage("Restart webserver"){
-        sh "ssh  ec2-user@34.254.229.99      sudo systemctl restart httpd "
+        sh "ssh  ec2-user@${Environment}      sudo systemctl restart httpd "
     }
 }
