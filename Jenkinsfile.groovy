1a0744d71c5f27f52cc83d563c35d8da9badcb1b
farrukh90 <farrukhsadykov@gmail.com>
2019-05-02 20:34:17 -0500

    environment changed to ENV

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,19 +1,19 @@
 node{
-    properties([parameters([string(defaultValue: 'IP', description: 'Where should I build? ', name: 'Environment', trim: true)]), pipelineTriggers([pollSCM('* * * * *')])])
+    properties([parameters([string(defaultValue: 'IP', description: 'Where should I build? ', name: 'ENV', trim: true)]), pipelineTriggers([pollSCM('* * * * *')])])
 
     stage("Pull Repo"){
         git 'git@github.com:farrukh90/cool_website.git'
     }
     stage("Webserver Install"){
-        sh "ssh  ec2-user@${Environment}       sudo yum install httpd -y"
+        sh "ssh  ec2-user@${ENV}       sudo yum install httpd -y"
     }
     stage("Index file"){
-        sh "scp  index.html          ec2-user@${Environment}:/tmp"
+        sh "scp  index.html          ec2-user@${ENV}:/tmp"
     }
     stage("Move index"){
-        sh "ssh  ec2-user@${Environment}     sudo mv /tmp/index.html        /var/www/html/index.html"
+        sh "ssh  ec2-user@${ENV}     sudo mv /tmp/index.html        /var/www/html/index.html"
     }
     stage("Restart webserver"){
-        sh "ssh  ec2-user@${Environment}      sudo systemctl restart httpd "
+        sh "ssh  ec2-user@${ENV}      sudo systemctl restart httpd "
     }
 }
