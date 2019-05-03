df68e060f4bb035961cc636c706705f8d8f33806
farrukh90 <farrukhsadykov@gmail.com>
2019-05-02 19:54:42 -0500

    added webserver

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,4 +2,7 @@ node{
     stage("Pull Repo"){
         git 'git@github.com:farrukh90/cool_website.git'
     }
+    stage("Webserver Install"){
+        sh "ssh  ec2-user@34.254.229.99       sudo yum install httpd -y"
+    }
 }
